f778fec6723b781a2f623b6e3a4880677f6fb580
sadagayathri <gayathrisada022@gmail.com>
2023-08-30 18:03:58 +0530

    vhjkh

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import Countriesapi from './Countriesapi';
+import Countriescard from './Countriescard';
 import Countriestable from './Countriestable';
+import Project from './Project';
 function App() {
   return (
     <div className="mybox">
       <Countriesapi></Countriesapi>
       <Countriestable></Countriestable>
+      <Countriescard></Countriescard>
+      <Project h={"Best of Paris in 7 days tour"} mat={"Paris is synonymous with the finest things that culture can offer — in art,fashion, food, literature, and ideas. On this tour, your Paris-savvy Rick Steves<br></br> guide will immerse you in the very best of"}></Project>
     </div>
   );
 }
