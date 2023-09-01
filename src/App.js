29c12f006daf645ef8e0a8adfc9219839cf31279
sadagayathri <gayathrisada022@gmail.com>
2023-09-01 17:16:06 +0530

    gfhhngm

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
-import Countriesapi from './Countriesapi';
-import Countriescard from './Countriescard';
-import Countriestable from './Countriestable';
+// import Countriesapi from './Countriesapi';
+// import Countriescard from './Countriescard';
+// import Countriestable from './Countriestable';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Shopping from './Shopping';
-import Project from './Project';
+// import Project from './Project';
 function App() {
   return (
     <div className="mybox">
       <Shopping></Shopping>
-      <Countriesapi></Countriesapi>
+      {/* <Countriesapi></Countriesapi>
       <Countriestable></Countriestable>
       <Countriescard></Countriescard>
-      <Project></Project>
+      <Project></Project> */}
       
     </div>
   );
