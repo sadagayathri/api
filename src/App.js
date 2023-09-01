613cabefb9f007b8794480cf863cbff2f2cd551c
sadagayathri <gayathrisada022@gmail.com>
2023-09-01 15:19:58 +0530

    jhiokk

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
-import Countriesapi from './Countriesapi';
-import Countriescard from './Countriescard';
-import Countriestable from './Countriestable';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Shopping from './Shopping';
-import Project from './Project';
-import Movie from './Movie';
+// import Countriesapi from './Countriesapi';
+// import Countriescard from './Countriescard';
+// import Countriestable from './Countriestable';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import Shopping from './Shopping';
+// import Project from './Project';
+// import Movie from './Movie';
 import Quiz from './Quiz';
 function App() {
   return (
     <div className="mybox">
       <Quiz></Quiz>
-      <Movie></Movie>
+      {/* <Movie></Movie>
       <Shopping></Shopping>
       <Countriesapi></Countriesapi>
       <Countriestable></Countriestable>
       <Countriescard></Countriescard>
-      <Project></Project>
+      <Project></Project> */}
       
     </div>
   );
--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 function Quiz() {
+     var [n,setN]=React.useState([10])
+     var inp=React.useRef()
+     console.log( inp.current)
+     React.useEffect(function(){
+        inp.current.value=10
+     },[])
     return (
-        <div>
+        <center>
+                    <div className='mybox1'>
             <h1>Setup Quiz</h1>
+            
+            <p>Number of questions</p>
+            <input type="number"  ref={inp} style={{width:"300px"}}></input>
+            <p>Category</p>
+            <select style={{width:"300px"}}>
+                <option>sports</option>
+                <option value="">history</option>
+                <option value="">politics</option>
+            </select>
+            <p>Select Difficulty</p>
+            <select style={{width:"300px"}}>
+                <option>easy</option>
+                <option value="">medium</option>
+                <option value="">hard</option>
+            </select>
+            <br></br>
+            <br></br>
+            
+            <button style={{width:"300px"}}>Start</button>
         </div>
+        </center>
     );
 }
 
